44f5036f911acfdef2e3d63551e3a9dba97c7d08
KeviTV <zxcvbnm20042005@gmail.com>
2022-04-16 22:27:45 +0300

    feat: version packet handler

--- src/main/java/ru/obvilion/home/devices/UnauthorizedDevice.java
+++ src/main/java/ru/obvilion/home/devices/UnauthorizedDevice.java
@@ -1,6 +1,8 @@
 package ru.obvilion.home.devices;
 
+import ru.obvilion.home.packets.AuthorizationPacket;
 import ru.obvilion.home.packets.PacketHandler;
+import ru.obvilion.home.packets.VersionPacket;
 import ru.obvilion.home.socket.ClientConnection;
 
 import java.util.HashMap;
@@ -8,6 +10,14 @@ import java.util.Map;
 
 public class UnauthorizedDevice extends Device {
     public static HashMap<Integer, PacketHandler> packet_handlers = new HashMap<>();
+
+    static {
+        packet_handlers.put(128, new AuthorizationPacket());
+        packet_handlers.put(129, new VersionPacket());
+    }
+
+    public short[] private_keys = { 1, 2, 3, 4 };
+    public short[] public_keys = { 1, 2, 3, 4 };
 
     public UnauthorizedDevice(ClientConnection connection) {
         super(connection);
---
+++ src/main/java/ru/obvilion/home/packets/VersionPacket.java
@@ -0,0 +1,30 @@
+package ru.obvilion.home.packets;
+
+import ru.obvilion.home.devices.Device;
+import ru.obvilion.home.devices.UnauthorizedDevice;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class VersionPacket implements PacketHandler {
+    @Override
+    public void handle(Device device, DataInputStream is, int length) throws IOException {
+        if (!(device instanceof UnauthorizedDevice)) {
+            return;
+        }
+
+        int size = is.read();
+        byte[] device_bytes = new byte[size];
+
+        is.read(device_bytes);
+
+        String device_name = new String(device_bytes, StandardCharsets.US_ASCII);
+        int version = is.readUnsignedShort();
+    }
+
+    @Override
+    public int getMinLength() {
+        return 4;
+    }
+}
